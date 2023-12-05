ca5387729d9255c191fb6c0acb52dca785e556c8
Maruf-dev <https://github.com/Maruf-dev>
2023-12-05 21:08:38 +0500

    add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-<<<<<<< HEAD
 
-=======
 import { StickyNavbar } from "./components/Navbar"
->>>>>>> 7319b4a40b0b06a9087d4fa6accada38d7275028
+import {BookingCard} from "./components/Card/Card"
+
+import { CarouselCustomArrows } from "./components/Carousel/carousel";
+
+
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
+
       
 
-=======
+
       <StickyNavbar/>
->>>>>>> 7319b4a40b0b06a9087d4fa6accada38d7275028
+      <BookingCard />
+      <CarouselCustomArrows />
+
     </>
   )
 }
