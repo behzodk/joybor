d87e0b1f30f9814f29380823d2f7a3caafaab7da
Behzod Musurmonqulov <behzodmusurmonqulov@gmail.com>
2023-12-11 19:33:26 +0500

    Behzod version 00.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import { StickyNavbar } from "./components/Navbar"
-import Hero from "./components/Hero"
-import {BookingCard} from "./components/Card/Card"
+import { useEffect, useState } from "react";
+import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
 
-import { CarouselCustomArrows } from "./components/Carousel/carousel";
+builder.init('f2f221044c6540cb91f4737222c30566');
 
-function App() {
+export default function CatchAllRoute() {
+  const isPreviewingInBuilder = useIsPreviewing();
+  const [notFound, setNotFound] = useState(false);
+  const [content, setContent] = useState(null);
 
+   useEffect(() => {
+    async function fetchContent() {
+      const content = await builder
+        .get("page", {
+          url: window.location.pathname
+        })
+        .promise();
+
+      setContent(content);
+      setNotFound(!content);
+      if (content?.data.title) {
+       document.title = content.data.title
+      }
+    }
+    fetchContent();
+  }, [window.location.pathname]);
+  if (notFound && !isPreviewingInBuilder) {
+    return <FourOhFour/>
+  }
   return (
     <>
-      <StickyNavbar/>
-      <Hero />
-      {/* <BookingCard /> */}
-      {/* <CarouselCustomArrows /> */}
+      <BuilderComponent model="page" content={content} />
     </>
-  )
+  );
 }
-
-export default App
